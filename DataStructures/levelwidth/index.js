dc176810a4a03c47aa35b3f123bc1d875e71c1b9
Jingyu Xiang <jxiang2@ualberta.ca>
2022-02-13 21:50:36 -0700

    tree implemented

--- DataStructures/levelwidth/index.js
+++ DataStructures/levelwidth/index.js
@@ -12,17 +12,18 @@
 // Answer: [1, 3, 2]
 
 function levelWidth(root) {
-	const arr = [root, "s"];
+	const arr = [root, "->"];
 	const widths = [0];
 
 	while (arr.length > 1) {
 		const node = arr.shift();
-		if (node === "s") {
+		if (node === "->") {
+			console.log("ready to process next level");
+			arr.push("->");
 			widths.push(0);
-			arr.push("s");
 		} else {
-			arr.push(...node.children);
 			widths[widths.length - 1]++;
+			arr.push(...node.children);
 		}
 	}
 
