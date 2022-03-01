f1f2e8382810225f2ef0bf5d9df242dddde5965a
Jingyu Xiang <jxiang2@ualberta.ca>
2022-03-01 11:43:47 -0700

    reformat

--- JavaScript/DOMevents/index.js
+++ JavaScript/DOMevents/index.js
@@ -5,33 +5,33 @@
 
 class Events {
 
-  constructor() {
-    this.events = {}
+  constructor () {
+    this.events = {};
   }
 
   // Register an event handler
-  on(eventName, callback) {
-    if (this.events[eventName]) {
-      this.events[eventName].push(callback)
+  on ( eventName, callback ) {
+    if ( this.events[ eventName ] ) {
+      this.events[ eventName ].push( callback );
     } else {
-      this.events[eventName] = [callback]
+      this.events[ eventName ] = [ callback ];
     }
   }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {
-    if (this.events[eventName]) {
-      for (let cb of this.events[eventName]) {
-        cb()
+  trigger ( eventName ) {
+    if ( this.events[ eventName ] ) {
+      for ( let cb of this.events[ eventName ] ) {
+        cb();
       }
     }
   }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {
-    delete this.events[eventName]
+  off ( eventName ) {
+    delete this.events[ eventName ];
   }
 }
 
--- JavaScript/useful-methods/equals.js
+++ JavaScript/useful-methods/equals.js
@@ -1,10 +1,10 @@
 // check if 2 arries are equal
-const arr1 = [1, 2, 3, 4, 5, "hello"];
-const arr2 = [1, 2, 3, 4, 5, "hello"];
+const arr1 = [ 1, 2, 3, 4, 5, "hello" ];
+const arr2 = [ 1, 2, 3, 4, 5, "hello" ];
 
-console.log(arr2.includes("5"));
+console.log( arr2.includes( "5" ) );
 
-console.log(JSON.stringify(arr1) === JSON.stringify(arr2));
+console.log( JSON.stringify( arr1 ) === JSON.stringify( arr2 ) );
 
 // check if 2 objects are equal
 const obj1 = {
@@ -15,7 +15,7 @@ const obj2 = {
 	name: "xjy",
 	age: 23,
 };
-console.log(JSON.stringify(obj1) === JSON.stringify(obj2));
+console.log( JSON.stringify( obj1 ) === JSON.stringify( obj2 ) );
 
 // check object length
 const obj3 = {
@@ -23,7 +23,7 @@ const obj3 = {
 	property2: 2,
 	property3: 3,
 };
-console.log(Object.keys(obj3).length);
+console.log( Object.keys( obj3 ).length );
 
 // check if 2 objects have the same keys
 const obj4 = {
@@ -36,4 +36,4 @@ const obj5 = {
 	p2: 6,
 	p3: "3",
 };
-console.log(JSON.stringify(Object.keys(obj4)) === JSON.stringify(Object.keys(obj5)));
+console.log( JSON.stringify( Object.keys( obj4 ) ) === JSON.stringify( Object.keys( obj5 ) ) );
