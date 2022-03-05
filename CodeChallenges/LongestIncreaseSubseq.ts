8c0148a078bfdf9af72f4f95de8f0e088b040921
Jxiang2 <jxiang2@ualberta.ca>
2022-03-05 13:20:55 -0700

    done

--- CodeChallenges/LongestIncreaseSubseq.ts
+++ CodeChallenges/LongestIncreaseSubseq.ts
@@ -2,25 +2,25 @@
  * Given an integer array nums, return the length of the 
  * longest strictly increasing subsequence.
  * 
- * For example, [3,6,2,7] is a subsequence of 
- * the array [0,3,1,6,2,2,7].
+ * Input: nums = [10,9,2,5,3,7,101,18]
+ * Output: 4
+ * Explanation: The longest increasing subsequence is [2,3,7,101],
+ *  therefore the length is 4.
  */
 
 function lengthOfLIS (nums: number[]): number {
-
-    if (nums.length < 1) return -1;
-
-    // each index store lengths of subsequences ended at nums[index]
     let dp = new Array(nums.length).fill(1);
-
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[j] > nums[i]) {
-                dp[j] = Math.max(nums[j], nums[i] + 1);
+    let prevs = new Array(nums.length).fill(-1);
+    for (let i = 1; i < nums.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[j] < nums[i]) {
+                dp[i] = Math.max(dp[i], dp[j] + 1);
+                prevs[i] = j;
             }
         }
     }
+    console.log(prevs);
     return Math.max(...dp);
 };
 
-console.log(lengthOfLIS([1, 2, 4, 2, 4, 5, 9, 2, 10]));
+console.log(lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]));
