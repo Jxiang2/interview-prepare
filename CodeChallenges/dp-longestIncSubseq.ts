c38af7cdfa007f696c4f7b89b0d392baa0cb6cd3
Jingyu Xiang <jxiang2@ualberta.ca>
2022-03-06 17:37:52 -0700

    rename

--- CodeChallenges/dp-longestIncSubseq.ts
+++ CodeChallenges/dp-longestIncSubseq.ts
@@ -10,16 +10,13 @@
 
 function lengthOfLIS (nums: number[]): number {
     let dp = new Array(nums.length).fill(1);
-    let prevs = new Array(nums.length).fill(-1);
     for (let i = 1; i < nums.length; i++) {
         for (let j = 0; j < i; j++) {
             if (nums[j] < nums[i]) {
                 dp[i] = Math.max(dp[i], dp[j] + 1);
-                prevs[i] = j;
             }
         }
     }
-    console.log(prevs);
     return Math.max(...dp);
 };
 
