e84cabb6aa3612122ca378b1cc1a6c065fd4b128
Jingyu Xiang <jxiang2@ualberta.ca>
2022-03-10 12:52:43 -0700

    done

--- CodeChallenges/dp-longestIncSubseq.java
+++ CodeChallenges/dp-longestIncSubseq.java
@@ -6,21 +6,18 @@ class LongestIncSubseq {
     public int lengthOfLIS(int[] nums) {
         int[] dp = new int[nums.length];
         Arrays.fill(dp, 1);
-
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
+            for (int j = 0; j < i; j++) {
                 if (nums[j] < nums[i]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
         }
-
         int max = dp[0];
         for (int i = 0; i < dp.length; i++) {
             if (dp[i] > max)
                 max = dp[i];
         }
-
         return max;
     }
 }
@@ -28,7 +25,7 @@ class LongestIncSubseq {
 class RunLongestIncSubseq {
     public static void main(String[] args) {
         LongestIncSubseq sol = new LongestIncSubseq();
-        int res = sol.lengthOfLIS(new int[] { 10, 9, 2, 5, 3, 7, 101, 18 });
+        int res = sol.lengthOfLIS(new int[] { 3, 2, 1 });
         System.out.println(res);
     }
 }
