4a020d3200f682d51046e019bbee50884fc70262
Jxiang2 <jxiang2@ualberta.ca>
2022-03-11 21:55:10 -0700

    done

--- CodeChallenges/math-rvsInteger.java
+++ CodeChallenges/math-rvsInteger.java
@@ -1,5 +1,8 @@
 package CodeChallenges;
 
+// The int will be implicitly converted to a long, 
+// which can always be done without any loss of information.
+
 class rvsInteger {
 
     int reverse(int x) {
