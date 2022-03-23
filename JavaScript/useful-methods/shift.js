a38f1c2f5d8158727215a2c6daee1d8254a43828
Jingyu Xiang <jxiang2@ualberta.ca>
2022-03-23 00:02:28 -0600

    reformat

--- JavaScript/useful-methods/shift.js
+++ JavaScript/useful-methods/shift.js
@@ -1,10 +1,10 @@
-let arr = [ 1, 2, 3, 4, 5 ];
+let arr = [1, 2, 3, 4, 5];
 
 // remove and return the element at index 0 of an array
-console.log( arr.shift() );
-console.log( arr );
+console.log(arr.shift());
+console.log(arr);
 
 // add elements at the beginning of an array
-let toAdd = [ 0, 1 ];
-arr.unshift( ...toAdd );
-console.log( arr );
+let toAdd = [0, 1];
+arr.unshift(...toAdd);
+console.log(arr);
--- JavaScript/useful-methods/sort.js
+++ JavaScript/useful-methods/sort.js
@@ -1,11 +1,11 @@
-const points = [ 40, 100, 1, 5, 25, 10 ];
+const points = [40, 100, 1, 5, 25, 10];
 
 // Sort the numbers in ascending order
-points.sort( ( a, b ) => a - b );
-let lowest = points[ 0 ];
-console.log( lowest );
+points.sort((a, b) => a - b);
+let lowest = points[0];
+console.log(lowest);
 
 // Sort the numbers in descending order
-points.sort( ( a, b ) => b - a );
-let heighest = points[ 0 ];
-console.log( heighest );
+points.sort((a, b) => b - a);
+let heighest = points[0];
+console.log(heighest);
