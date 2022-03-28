73f85e9314646c118be91a7075fea99e4de012cb
Jxiang2 <jxiang2@ualberta.ca>
2022-03-27 19:38:11 -0600

    update js

---
+++ JavaScript/basics/thisKeyWord2.js
@@ -0,0 +1,36 @@
+// arrow function doesn't define its own execution context (it does not have it's own this )
+// They inherit this from it' parent scope
+
+// regular function has it's defined this,
+// if it's invoked by it's non-defualt object (call apply bind), this changes.
+
+module.exports.foo = 5;
+
+console.log(this);
+let f1 = () => {
+    console.log(this);
+};
+let f2 = function () {
+    console.log(this);
+};
+
+f1(); // f1 does not has this, it uses the module as it's this
+f2(); // f2 has it's own scope "global", which is auto-set when a function is created
+
+let obj = {
+    a: 1,
+    b: 2,
+    c: () => {
+        console.log(this);
+    },
+    d: function () {
+        console.log(this);
+    }
+};
+
+obj.c(); // c does not has this, it uses this from it's parent scope, which is the module.export object
+obj.d(); // d is defined in the obj, so it's excution context is the obj, thus this is obj
+
+
+
+
