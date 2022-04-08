4640994a95c0f2d6141e5fd069acda9ddc79d72d
Jingyu Xiang <jxiang2@ualberta.ca>
2022-04-08 15:14:42 -0600

    rename

--- Algorithms/capitalize/index.py
+++
@@ -1,9 +0,0 @@
-def capitalize(str):
-    wordList = []
-    for word in str.split(' '):
-        capitalizedWord = word[0].upper() + word[1:]
-        wordList.append(capitalizedWord)
-    return ' '.join(wordList)
-
-
-print(capitalize('a short sentence'))
--- Algorithms/reversestring/index.py
+++
@@ -1,8 +0,0 @@
-def reverse(str):
-    reversedList = []
-    for ch in str:
-        reversedList.insert(0, ch)
-    return ''.join(reversedList)
-
-
-print(reverse('Greetings!'))
