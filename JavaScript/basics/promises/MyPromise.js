5685181c1db3398cdbae95b5eb721ac78b4135bc
Jxiang2 <xjyhenry@gmail.com>
2022-06-20 22:18:04 -0400

    refine myPromise

--- JavaScript/basics/promises/MyPromise.js
+++ JavaScript/basics/promises/MyPromise.js
@@ -1,7 +1,22 @@
+/**
+ * When a new Promise is initialized,
+ * it's aleady resolved or rejected.
+ * We just need to use then() or catch() to
+ * get the resolved or rejected result
+ */
+
 const STATE = {
   FULLFILLED: "fullfilled",
   REJECTED: "rejected",
   PEDNING: "pending"
+}
+
+class UncaughtPromiseError extends Error {
+  constructor (error) {
+    super(error)
+
+    this.stack = `(in promise) ${error.stack}`
+  }
 }
 
 class MyPromise {
@@ -14,24 +29,24 @@ class MyPromise {
 
   constructor (promiseCb) {
     try {
-      promiseCb(this.#onSuccessBinded, this.#onFailBinded)
+      promiseCb(this.#onSuccessBinded, this.#onFailBinded) // call (resolve, reject) => ... when new a Promise
     } catch (err) {
       this.onFail(err)
     }
   }
 
-  hello () {
-    console.log(this)
-  }
-
   #runCallbacks () {
     if (this.#state === STATE.FULLFILLED) {
       this.#thenCbs.forEach(callback => callback(this.#value))
+
+      // make sure excuted callbacks are no longer excuted again
       this.#thenCbs = []
     }
 
     if (this.#state === STATE.REJECTED) {
       this.#catchCbs.forEach(callback => callback(this.#value))
+
+      // make sure excuted callbacks are no longer excuted again
       this.#catchCbs = []
     }
   }
@@ -39,11 +54,15 @@ class MyPromise {
   #onSuccess (value) {
     if (this.#state !== STATE.PEDNING) return
 
+    // value is a promise
     if (value instanceof MyPromise) {
       value.then(this.#onSuccessBinded, this.#onFailBinded)
     }
 
+    // value is a promise
     this.#value = value
+
+    // value is not a promise
     this.#state = STATE.FULLFILLED
 
     this.#runCallbacks()
@@ -52,37 +71,51 @@ class MyPromise {
   #onFail (value) {
     if (this.#state !== STATE.PEDNING) return
 
+    // value is a promise
     if (value instanceof MyPromise) {
       value.then(this.#onSuccessBinded, this.#onFailBinded)
     }
 
+    // if error occurs but no .catch() provided, throw erro
+    if (this.#catchCbs.length === 0) {
+      throw new UncaughtPromiseError(value)
+    }
+
+    // value is not a promise
     this.#value = value
+
     this.#state = STATE.REJECTED
 
     this.#runCallbacks()
   }
 
+  /**
+   * @param thenCb
+   * @param catchCb 
+   * @returns MyPromise
+   */
   then (thenCb, catchCb) {
     return new MyPromise((resolve, reject) => {
-      this.#thenCbs.push(result => {
-        if (thenCb === null) {
-          resolve(result)
+      this.#thenCbs.push(result => { // we can do multiple p.then() by pushing functions to thenCbs array
+        if (thenCb === null) { // if callback is not provided in then(), skip this then()
+          resolve(result) // call #onSuccess in the sub MyPromise and set #value to the #value in current MyPromise
           return
         }
         try {
-          resolve(thenCb(result))
+          resolve(thenCb(result)) // resolve the result of function passed in previous then()
         } catch (err) {
           reject(err)
         }
       })
 
-      this.#catchCbs.push(result => {
-        if (catchCb === null) {
-          reject(result)
+
+      this.#catchCbs.push(result => { // we can do multiple p.catch() by pushing functions to catchCbs array
+        if (catchCb === null) { // if callback is not provided in catch(), skip this catch()
+          reject(result) // call #onFail in the sub MyPromise and set #value to the #value in current MyPromise
           return
         }
         try {
-          resolve(catchCb(result))
+          resolve(catchCb(result)) // resolve the result of function passed in previous catch()
         } catch (err) {
           reject(err)
         }
@@ -92,9 +125,36 @@ class MyPromise {
     })
   }
 
+  /**
+   * @param catchCb 
+   * @returns MyPromise
+   */
   catch (catchCb) {
     return this.then(undefined, catchCb)
+  }
+
+  /**
+   * @param finallyCb 
+   * @returns  MyPromise
+   */
+  finally (finallyCb) { // finally passes result to next chain and finally callback does not take parameters 
+    return this.then(result => {
+      finallyCb()
+      return result
+    }, result => {
+      finallyCb()
+      throw result
+    })
   }
 }
 
 module.exports = MyPromise
+
+// tests
+let p = new Promise((resolve, reject) => {
+  resolve("yay!")
+})
+p
+  .then(data => data.toString())
+  .then(res => res.toUpperCase())
+  .then(res => console.log(res))
