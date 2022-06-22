0a2ddfa8156c1711fbff774e03224b32ce32e28b
Jxiang2 <xjyhenry@gmail.com>
2022-06-21 23:24:35 -0400

    review debounce and throttle

---
+++ JavaScript/useful-methods/frontend/debounce.js
@@ -0,0 +1,16 @@
+/**
+ * debounce def:
+ * excute the event at the last click
+ * the button while constantly clicking it
+ */
+const debounce = (fn, delay) => {
+  let timeOutId
+  return (...args) => {
+    timeOutId && clearTimeout(timeOutId)
+    timeOutId = setTimeout(() => fn(...args), delay)
+  }
+}
+
+document.getElementById("btn1").addEventListener("click", debounce(e => {
+  console.log("clicked")
+}, 2000))
---
+++ JavaScript/useful-methods/frontend/throttle.js
@@ -0,0 +1,21 @@
+/**
+ * throttle def:
+ * excute the event on the first click
+ * within the throttle delay, the event won't fire again
+ */
+const throttle = (fn, delay) => {
+  let last = 0
+  return (...args) => {
+    const now = new Date().getTime()
+    if (now - last < delay) {
+      return
+    }
+    last = now
+    return fn(...args)
+  }
+}
+
+
+document.getElementById("btn1").addEventListener("click", debounce(e => {
+  console.log("clicked")
+}, 2000))
