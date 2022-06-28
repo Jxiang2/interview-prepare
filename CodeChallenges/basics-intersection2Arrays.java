935d6ff7de7796178da062aec5a4cef4378c19b2
Jxiang2 <xjyhenry@gmail.com>
2022-06-28 17:04:07 -0400

    update challenge

---
+++ CodeChallenges/basics-intersection2Arrays.java
@@ -0,0 +1,30 @@
+package CodeChallenges;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class IntersectionArrays {
+  public List<Integer> intersect(int[] numbers1, int[] numbers2) {
+    Arrays.sort(numbers1);
+    Arrays.sort(numbers2);
+
+    int p1 = 0;
+    int p2 = 0;
+    List<Integer> res = new ArrayList<>();
+
+    while (p1 < numbers1.length && p2 < numbers2.length) {
+      if (numbers1[p1] == numbers2[p2]) {
+        res.add(numbers1[p1]);
+        p1++;
+        p2++;
+      } else if (numbers2[p2] > numbers1[p1]) {
+        p1++;
+      } else {
+        p2++;
+      }
+    }
+
+    return res;
+  }
+}
