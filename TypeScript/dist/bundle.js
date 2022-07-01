b1440e99501aa2fb21e730c22ec8812cf5ee8f94
Jxiang2 <xjyhenry@gmail.com>
2022-07-01 11:22:44 -0400

    webpack done

--- TypeScript/dist/bundle.js
+++ TypeScript/dist/bundle.js
@@ -1,2 +1,157 @@
-(()=>{"use strict";({84:function(){var o=this&&this.__assign||function(){return o=Object.assign||function(o){for(var e,n=1,l=arguments.length;n<l;n++)for(var a in e=arguments[n])Object.prototype.hasOwnProperty.call(e,a)&&(o[a]=e[a]);return o},o.apply(this,arguments)},e=[];e.push("hello"),e.push("world"),console.log("characters: ",e);var n,l,a,s,r=[];r.push("hello"),r.push(20),console.log("mixed:",r),n=13,console.log(n),n="12345",console.log(n),l={name:"xjy",age:22,belt:"black"},console.log(l),s={name:"xjy",uid:1537572},console.log("".concat(s.name," | ").concat(s.uid)),void 0===a&&(a=20),console.log(15+Number(a));var t;console.log(3),t={name:"xjy",age:30,speak:function(o){console.log(o)},spend:function(o){return console.log("I spent",o),o}},console.log("hello",t.name);var c=function(e){var n=Math.floor(100*Math.random());return o(o({},{name:"yoshi",age:40}),{uid:n})}();console.log(c.age);var g=function(o,e){console.log(o)};g("hello"),g(1),console.log({uid:12345,resourceName:"person",data:"string data"}),console.log({uid:12345,resourceName:"person",data:["string data 1","string data 2"]}),console.log({k1:1,k2:2,k3:5,helloWord:15});var i,u,d={id:"3d2ce32",birthDate:new Date(Date.now()),tropies:[]};console.log(d),(i={}).age=12,i.name="hello",console.log(i),function(o){o[o.BOOK=0]="BOOK",o[o.AUTHOR=1]="AUTHOR",o[o.FILM=2]="FILM",o[o.DIRECTOR=3]="DIRECTOR",o[o.PERSON=4]="PERSON"}(u||(u={})),console.log(u.AUTHOR),console.log({id:"3r23f0jewfewf",name:"xjy",age:12,isMarried:!1})}})[84]()})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/summary.ts":
+/*!************************!*\
+  !*** ./src/summary.ts ***!
+  \************************/
+/***/ (function() {
+
+
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+// arrays
+var characters = [];
+characters.push('hello');
+characters.push('world');
+console.log('characters: ', characters);
+// unions for multi-type variables
+var mixed = [];
+mixed.push('hello');
+mixed.push(20);
+console.log('mixed:', mixed);
+var uid;
+uid = 13;
+console.log(uid);
+uid = '12345';
+console.log(uid);
+// objects, the properties can not be changed once defined
+var ninjaOne;
+ninjaOne = { name: 'xjy', age: 22, belt: 'black' };
+console.log(ninjaOne);
+var greet;
+var add;
+var minus;
+greet = function (user) {
+    console.log("".concat(user.name, " | ").concat(user.uid));
+};
+greet({ name: 'xjy', uid: 1537572 });
+add = function (a, b, c) {
+    if (c === void 0) { c = 20; }
+    console.log(a + b + Number(c));
+};
+add(5, 10);
+minus = function (a, b) {
+    return a - b;
+};
+var result = minus(10, 7);
+console.log(result);
+var me = {
+    name: 'xjy',
+    age: 30,
+    speak: function (text) {
+        console.log(text);
+    },
+    spend: function (amount) {
+        console.log('I spent', amount);
+        return amount;
+    }
+};
+var greetperson = function (person) {
+    console.log('hello', person.name);
+};
+greetperson(me);
+// generics
+// capture whatever item we pass in the function
+// I need to pass a object with a property called name
+var addUID = function (obj) {
+    var uid = Math.floor(Math.random() * 100);
+    return __assign(__assign({}, obj), { uid: uid });
+};
+var docOne = addUID({ name: 'yoshi', age: 40 });
+console.log(docOne.age);
+// generics
+// specify the type of input when calling a generic function
+var printAny = function (state, name) {
+    console.log(state);
+};
+printAny("hello", "me");
+printAny(1, "me");
+var docThree = {
+    uid: 12345,
+    resourceName: 'person',
+    data: 'string data'
+};
+var docFour = {
+    uid: 12345,
+    resourceName: 'person',
+    data: ['string data 1', 'string data 2']
+};
+console.log(docThree);
+console.log(docFour);
+;
+var var1 = true;
+var var2 = true;
+var var3 = false;
+var id1 = {
+    k1: 1,
+    k2: 2,
+    k3: 5,
+    helloWord: 15
+};
+console.log(id1);
+;
+var optionalObj = {
+    id: "3d2ce32",
+    birthDate: new Date(Date.now()),
+    tropies: []
+};
+console.log(optionalObj);
+var hello;
+hello = {};
+hello.age = 12;
+hello.name = "hello";
+console.log(hello);
+// enums : 0, 1, ,2, 3, 4
+var ResourceType;
+(function (ResourceType) {
+    ResourceType[ResourceType["BOOK"] = 0] = "BOOK";
+    ResourceType[ResourceType["AUTHOR"] = 1] = "AUTHOR";
+    ResourceType[ResourceType["FILM"] = 2] = "FILM";
+    ResourceType[ResourceType["DIRECTOR"] = 3] = "DIRECTOR";
+    ResourceType[ResourceType["PERSON"] = 4] = "PERSON";
+})(ResourceType || (ResourceType = {}));
+console.log(ResourceType.AUTHOR);
+var mixedBase = {
+    id: "3r23f0jewfewf",
+    name: "xjy",
+    age: 12,
+    isMarried: false
+};
+console.log(mixedBase);
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/summary.ts"]();
+/******/ 	
+/******/ })()
+;
 //# sourceMappingURL=bundle.js.map
--- TypeScript/webpack.config.js
+++ TypeScript/webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path')
 
-module.exports = {
-  mode: 'production',
+const MODE = 'production'
 
-  devtool: 'source-map', // to better debug on browser
+module.exports = {
+  mode: MODE || 'development',
+
+  devtool: MODE === 'production' ? 'source-map' : 'eval-source-map', // to better debug on browser
 
   entry: './src/summary.ts', //relative path of entry file; all other paths in this file will be absolute
 
@@ -19,13 +21,21 @@ module.exports = {
         // if a file in src folder is ending with .ts, using ts-loader on it
         test: /\.ts$/,
         use: 'ts-loader',
-        include: [path.resolve(__dirname, 'src')]
+        include: [path.resolve(__dirname, 'src')],
       },
     ],
   },
 
   resolve: {
     // specify estensions for imports
-    extensions: ['.ts', '.js',]
+    extensions: ['.ts', '.js',],
   },
+
+  devServer: { // development hot-reload mode
+    static: {
+      directory: path.resolve(__dirname, 'dist')
+    },
+    compress: true,
+    port: 9000,
+  }
 }
