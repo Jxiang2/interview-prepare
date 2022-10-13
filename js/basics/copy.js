274eee920246d8c0481a81cc221cb1121421b987
Jxiang2 <xjyhenry@gmail.com>
2022-10-13 17:51:40 -0400

    update copy

--- js/basics/copy.js
+++ js/basics/copy.js
@@ -1,11 +1,11 @@
 const a = {
   name: "xjy",
   age: 23,
-  ids: [1, 2, 3]
+  ids: [{ type: "1", code: 1 }, { type: "2", code: 2 }, { type: "3", code: 3 }]
 }
 
 let b = { ...a }
-b.ids = [1, 2]
+b.ids = [{ type: "1", code: 1 }, { type: "2", code: 2 }]
 
 console.log(a)
 console.log(b)
@@ -13,7 +13,7 @@ console.log(b)
 console.log("###############")
 
 b = a
-b.ids = [1, 2]
+b.ids = [{ type: "1", code: 1 }, { type: "2", code: 2 }]
 
 console.log(a)
 console.log(b)
