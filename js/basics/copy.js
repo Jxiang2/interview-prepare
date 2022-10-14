eb5ae5f18a60902e3a630a2df4f1fcbcfa32306b
Jxiang2 <xjyhenry@gmail.com>
2022-10-13 22:04:12 -0400

    remove redundant deepcopies

--- js/basics/copy.js
+++ js/basics/copy.js
@@ -8,22 +8,25 @@ const a = {
   ],
 };
 
-let b = { ...a };
+let b = { ...a }; // shallow copy, b is at another location than a
+console.log(b.ids === a.ids); // b.ids and a.ids still point to the same array
 b.ids = [
   { type: "1", code: 1 },
   { type: "2", code: 2 },
 ];
+console.log(b.ids === a.ids);
 
 console.log(a);
 console.log(b);
 
 console.log("###############");
 
-b = a;
+b = a; // b and a point to the same
 b.ids = [
   { type: "1", code: 1 },
   { type: "2", code: 2 },
 ];
+console.log(b.ids === a.ids);
 
 console.log(a);
 console.log(b);
