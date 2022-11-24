6ddf538252facfe4b56040b6d37776cab0b7053f
Jxiang2 <xjyhenry@gmail.com>
2022-11-23 20:44:31 -0500

    compare

--- js/packages/lodash/omit.js
+++ js/packages/lodash/omit.js
@@ -1,10 +1,35 @@
 const omit = require("lodash/omit");
+const isEqual = require("lodash/isEqual");
 
 // The source object
-var obj = {
+let obj = {
   Name: "GeeksforGeeks",
   password: "gfg@1234",
   username: "your_geeks",
 };
 
-console.log(omit(obj));
+const omitted = omit(obj);
+
+console.log(omitted === obj);
+console.log(omitted);
+console.log(obj);
+
+let obja = {
+  a: 1,
+  b: {
+    c: 2,
+    d: 3,
+  },
+  e: [{ f: 1 }, { f: 2 }, { f: 3 }],
+};
+let objb = {
+  a: 1,
+  b: {
+    c: 2,
+    d: 3,
+  },
+  e: [{ f: 3 }, { f: 4 }, { f: 5 }],
+};
+
+// deep comparison
+console.log(isEqual(obja, objb));
