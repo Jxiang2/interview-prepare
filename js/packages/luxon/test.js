0ea7cbb81e9e3e2c406a41ce9c0b852b3a7170c0
Jxiang2 <xjyhenry@gmail.com>
2023-01-18 15:25:26 -0500

    update luxon

--- js/packages/luxon/test.js
+++ js/packages/luxon/test.js
@@ -1,15 +1,27 @@
 // Montreal timezone: UTC-5 ===> Montreal time + 5h = UTC time
+// 2022-12-17    T     03:50:00          +0000
+//   date                time        offset(timezonex)
 
 const { DateTime } = require("luxon");
 
-// gives Datetime object in local timezone
+// give a Datetime object in local timezone
 const dateTime1 = DateTime.fromISO("2022-12-17T03:50:00+0000");
 console.log(dateTime1);
 
-// gives Datetime object in specified timezone
+// give a Datetime object in specified timezone
 const dateTime2 = DateTime.fromISO("2022-12-17T03:50:00+0000", { zone: "utc" });
 console.log(dateTime2);
 const dateTime3 = DateTime.fromISO("2022-12-17T03:50:00+0000", {
   zone: "Europe/Kiev",
 });
 console.log(dateTime3);
+
+// give a Datetime object, do not change datetime values, but set a timezone to it
+const current = DateTime.fromISO("2022-12-25T00:00:00", {
+  zone: "Europe/Paris",
+});
+// The date and time specified in the current.toString() was interpreted as a Parisian local time
+// (i.e. it's the time that corresponds to what would be called 9:10 there).
+console.log(current);
+console.log(current.toString());
+console.log(current.toISODate());
