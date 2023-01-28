1bc0225bf85dc832d8db91c5404f06b9ffdc283c
Jxiang2 <xjyhenry@gmail.com>
2023-01-28 14:17:43 -0500

    update dp

--- code/dp/dp-fib.java
+++ code/dp/dp-fib.java
@@ -1,15 +1,27 @@
 package code.dp;
 
-//n:    1, 2, 3, 4, 5, 6, 7 ...
-//f(n): 1, 1, 2, 3, 5, 8, 13 ...
+import java.util.HashMap;
+import java.util.Map;
+
+// n:    0, 1, 2, 3, 4, 5, 6, 7 ...
+// f(n): 0, 1, 1, 2, 3, 5, 8, 13 ...
+// runtime: O(n)
 class Fib {
-  public int fibN(int n) {
-    if (n <= 2) return 1;
-    return fibN(n - 1) + fibN(n - 2);
+  public long fibN(int n, Map<Integer, Long> memo) {
+    if (memo.containsKey(n)) 
+      return memo.get(n);
+
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+
+    memo.put(n, fibN(n - 1, memo) + fibN(n - 2, memo));
+    return memo.get(n);
   }
 
   public static void main(String[] args) {
     Fib solution = new Fib();
-    System.out.println( solution.fibN(5));
+    System.out.println(
+      solution.fibN(50, new HashMap<Integer, Long>())
+    );
   }
 }
---
+++ code/dp/dp-gridtravel.java
@@ -0,0 +1,38 @@
+package code.dp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// how many ways travel from mxn top left to bottom right
+// if move only left and botton?
+// runtime: O(c+r)
+//    c
+// *  *  * 
+// *  *  *   r
+// *  *  *
+class GridTravel {
+
+  public long gridTravelWays(int r, int c, Map<String, Long> memo) {
+    String key = r + "," + c;
+    String reversedKey = c + "," + r;
+
+    if (memo.containsKey(key) || memo.containsKey(reversedKey)) {
+      return memo.getOrDefault(key, memo.get(reversedKey));
+    }
+
+    if (r == 0 || c == 0) return 0; // trivial
+    if (r == 1 || c == 1) return 1; // nopn-trivial
+    
+    long value = gridTravelWays(r-1, c, memo) + gridTravelWays(r, c-1, memo); // D + L
+    memo.put(key, value); 
+    return memo.get(key);
+  }
+
+  public static void main(String[] args) {
+    GridTravel gridTravel = new GridTravel();
+    System.out.println(
+      gridTravel.gridTravelWays(18,18, new HashMap<String, Long>())
+    );
+  }
+   
+}
