169dcabe4a7ff2cd875d42ddf88b53a393e0a07d
Jxiang2 <xjyhenry@gmail.com>
2023-01-28 20:08:31 -0500

    add

---
+++ algorithm/bsearch/bsearch.js
@@ -0,0 +1,17 @@
+function bsearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    mid = (left + right) / 2;
+    if (arr[mid] === target) {
+      return mid;
+    } else if (target < arr[mid]) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return -1;
+}
---
+++ datastructure/bst-validate/index.js
@@ -0,0 +1,29 @@
+// --- Directions
+// return true if bst is valid
+// return false if bst is not valid
+
+function validate(node, min = null, max = null) {
+  // idea: setup a minMax range
+
+  if (max !== null && node.data > max) {
+    return false;
+  }
+
+  if (min !== null && node.data < min) {
+    return false;
+  }
+
+  if (node.left && !validate(node.left, min, node.data)) {
+    // left node is not valid
+    return false;
+  }
+
+  if (node.right && !validate(node.right, node.data, max)) {
+    // left node is not valid
+    return false;
+  }
+
+  return true;
+}
+
+module.exports = validate;
