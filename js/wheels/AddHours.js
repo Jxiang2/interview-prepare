e792051d2d6875b9c90b450979c0d264d7fe8fe0
Jxiang2 <xjyhenry@gmail.com>
2023-01-31 17:23:36 -0500

    update

---
+++ js/wheels/AddHours.js
@@ -0,0 +1,30 @@
+/**
+ * Add hourses
+ * @param {string} hoursA - hh:mm:ss
+ * @param {string} hoursB - hh:mm:ss
+ * @param {string[]} hoursList - remaining hourses
+ * @returns string - hh:mm:ss
+ */
+function sumHourses(hoursA, hoursB, hoursList = []) {
+  const unitSeconds = [3600, 60, 1];
+
+  let sumInSeconds = [hoursA, hoursB, ...hoursList]
+    .map((time) =>
+      time.split(":").reduce((subSum, value, i) => {
+        return subSum + unitSeconds[i] * Number(value);
+      }, 0),
+    )
+    .reduce((a, b) => a + b, 0);
+
+  return unitSeconds
+    .map((unitSeconds) => {
+      const value = Math.floor(sumInSeconds / unitSeconds);
+      sumInSeconds %= unitSeconds;
+      return value;
+    })
+    .map((value) => value.toString().padStart(2, "0"))
+    .join(":");
+}
+
+const x = sumHourses("24:30:30", "00:30:30");
+console.log(x);
