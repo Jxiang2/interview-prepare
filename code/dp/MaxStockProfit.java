e06909d9e3a6304988cf755d68172549bdc64bdf
Jxiang2 <xjyhenry@gmail.com>
2023-02-10 22:43:49 -0500

    add

--- code/dp/MaxStockProfit.java
+++ code/dp/MaxStockProfit.java
@@ -29,6 +29,7 @@ class MaxStockProfit {
 
     /**
      * On each day, you may decide to buy and/or sell the stock.
+     * trick: count every vally and peak combo
      * 
      * Input: prices = [7,1,5,3,6,4]
      * Output: 7
@@ -38,11 +39,19 @@ class MaxStockProfit {
      * 3.
      * Total profit is 4 + 3 = 7.
      */
-    public int maxProfit2(int[] prices) {
+    public int maxProfit2(final int[] prices) {
+        int i = 0;
+        int valley = prices[0];
+        int peak = prices[0];
         int maxprofit = 0;
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i - 1])
-                maxprofit += prices[i] - prices[i - 1];
+        while (i < prices.length - 1) {
+            while (i < prices.length - 1 && prices[i] >= prices[i + 1])
+                i++;
+            valley = prices[i];
+            while (i < prices.length - 1 && prices[i] <= prices[i + 1])
+                i++;
+            peak = prices[i];
+            maxprofit = maxprofit + peak - valley;
         }
         return maxprofit;
     }
