ad74c8dbdddd42b713c97d9d0769d42424ae388f
Jxiang2 <xjyhenry@gmail.com>
2023-02-10 21:19:26 -0500

    add

--- code/dp/MaxStockProfit.java
+++ code/dp/MaxStockProfit.java
@@ -1,18 +1,18 @@
-/**
- * You are given an integer array prices where 
- * prices[i] is the price of a given stock on the ith day.
- * 
- * On each day, you may decide to buy and/or sell
- * the stock. You can only hold at most one share 
- * of the stock at any time. However, you can 
- * buy it then immediately sell it on the same day.
- * 
- * Find and return the maximum profit you can achieve.
- */
-
 package code.dp;
 
 class MaxStockProfit {
+
+    /**
+     * choosing a single day to buy one stock and choosing a different day in the
+     * future to sell that stock
+     * 
+     * Input: prices = [7,1,5,3,6,4]
+     * Output: 5
+     * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit =
+     * 6-1 = 5.
+     * Note that buying on day 2 and selling on day 1 is not allowed because you
+     * must buy before you sell.
+     */
     public int maxProfit(final int[] prices) {
         int minprice = Integer.MAX_VALUE;
         int maxprofit = 0;
@@ -24,6 +24,26 @@ class MaxStockProfit {
                 maxprofit = prices[i] - minprice;
         }
 
+        return maxprofit;
+    }
+
+    /**
+     * On each day, you may decide to buy and/or sell the stock.
+     * 
+     * Input: prices = [7,1,5,3,6,4]
+     * Output: 7
+     * Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit =
+     * 5-1 = 4.
+     * Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 =
+     * 3.
+     * Total profit is 4 + 3 = 7.
+     */
+    public int maxProfit2(int[] prices) {
+        int maxprofit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1])
+                maxprofit += prices[i] - prices[i - 1];
+        }
         return maxprofit;
     }
 }
