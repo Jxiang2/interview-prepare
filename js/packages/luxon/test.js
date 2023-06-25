b256c0e874f2ce32fe7803f6a0baa398135c80a5
Jxiang2 <xjyhenry@gmail.com>
2023-06-25 00:01:44 -0400

    add

--- js/packages/luxon/test.js
+++ js/packages/luxon/test.js
@@ -10,17 +10,18 @@ const dateTime1 = DateTime.fromISO("2022-12-17T03:50:00+0000"); // +0000 means U
 console.log(dateTime1);
 console.log(dateTime1.toISO());
 // ------------------------------------------------------
+console.log("--------------------------------------------");
 
 // return a Datetime object in specified timezone (c changed)
 const dateTime2 = DateTime.fromISO("2022-12-17T03:50:00+0000", {
   zone: "Europe/Paris", // UTC+1
 });
 console.log(dateTime2);
-const dateTime3 = DateTime.fromISO("2022-12-17T03:50:00+0000", {
-  zone: "Europe/Kiev", // UTC+2
-});
+
+const dateTime3 = dateTime2.setZone("Europe/Kiev"); // UTC+2
 console.log(dateTime3);
 // ------------------------------------------------------
+console.log("--------------------------------------------");
 
 // give a Datetime object, do not change datetime values, but set a timezone to it
 const current = DateTime.fromISO("2022-12-25T00:00:00", {
@@ -31,9 +32,10 @@ const current = DateTime.fromISO("2022-12-25T00:00:00", {
 console.log(current);
 console.log(current.toISO());
 // ------------------------------------------------------
+console.log("--------------------------------------------");
 
 // convert a Datetime to another timezone, but keeping the values the same (Full example)
-console.log("------------------------------------------------------");
+
 const dateTimeFromServer = "2023-01-23T00:00:00.000+00:00";
 
 const viewerLocalDate = DateTime.fromISO(dateTimeFromServer, {
@@ -56,6 +58,7 @@ const final = viewerLocalDate.set({
 
 console.log(final.toISO());
 // ------------------------------------------------------
+console.log("--------------------------------------------");
 
 // Create datetimes based on year, month, day, hour, minute, second, and timezone, or defailt system timezone
 const ldt1 = DateTime.fromObject(
@@ -84,3 +87,5 @@ console.log(ldt2);
 
 // Compare
 console.log(ldt1 < ldt2);
+
+console.log(DateTime.now().minus({ day: -1 }).weekday);
